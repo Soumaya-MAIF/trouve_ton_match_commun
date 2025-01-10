9e8a9fcd7ce77cf68bde4cd15d56f285741917d3
Jerome BOUHET <jerome.bouhet@maif.fr>
2025-01-10 15:22:58 +0100

    chore: initialistation du back

---
+++ back/src/test/java/back/trouve_ton_match/TrouveTonMatchApplicationTests.java
@@ -0,0 +1,13 @@
+package back.trouve_ton_match;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrouveTonMatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
