2c730b0c1d13d2a1d5a049b2ca5b9e1ba6f37447
Jerome BOUHET <jerome.bouhet@maif.fr>
2025-01-20 09:39:35 +0100

    feat: header de l'appli

---
+++ src/main/js/wrapper/Header.jsx
@@ -0,0 +1,80 @@
+import logo from '../../medias/images/logos/Nouvelle_Aquitaine-Logo-Reseau_Initiative.webp';
+import logo_TTM_vert from '../../medias/images/logos/logo_TTM_vert.png';
+import './header.css';
+import { Link } from "react-router-dom";
+// import { useAuth } from '../AuthContext';
+
+const Header = () => {
+    // const { isAuthenticated, logout } = useAuth();
+
+    return (
+        <header className="header">
+            <div className="header-container">
+                <div className="header-row">
+                    <div className="header-col">
+                        <Link to="/">
+                            <div className="favicon-container">
+                                <div className="favicon" alt="favicon_TTM"></div>
+                            </div>
+                        </Link>
+                    </div>
+                    <div className="header-col">
+                        <Link className="menu" to="/monCompteParrain">Mon compte</Link>
+                    </div>
+                    <div className="header-col">
+                        <Link className="menu" to="/profils">Profils disponibles</Link>
+                    </div>
+                    <div className="header-col">
+                        <Link className="menu" to="/messages">Messages</Link>
+                    </div>
+                    <div className="header-col">
+                        <Link className="menu" to="/matchs">Mes Matchs</Link>
+                    </div>
+                    <div className="header-col">
+                        <Link className="menu" to="/ressources">Ressources</Link>
+                    </div>
+                    <div className="header-col">
+                        <Link className="menu" to="/indicateurs">Indicateurs</Link>
+                    </div>
+                    <div className="header-col">
+                        <div className="bouton">
+                        {/* <button type="button" className="btn" onClick={logout}>Se déconnecter</button> */}
+                        <Link to="/connexion">
+                            <button type="button" className="btn-connexion">Se connecter</button>
+                        </Link>
+                            {/* {isAuthenticated ? (
+                                <button type="button" className="btn" onClick={logout}>Se déconnecter</button>
+                            ) : (
+                                <Link to="/connexion">
+                                    <button type="button" className="btn">Se connecter</button>
+                                </Link>
+                            )} */}
+                        </div>
+                    </div>
+                    <div className="header-col">
+                        <div className="bouton">
+                            <Link to="/creationCompte">
+                                <button type="button" className="btn-creer">Créer un compte</button>
+                            </Link>
+                        </div>
+                    </div>
+                </div>
+                <div className="header-row">
+                    <div className="header-col">
+                        <div className="logo-container">
+                            <img src={logo_TTM_vert} className="logo ttm" alt="logo_TTM" />
+                        </div>
+                    </div>
+                    <div className="header-col">
+                        <div className="logo-container">
+                            <img src={logo} className="logo" alt="logo" />
+                        </div>
+                    </div>
+                </div>
+                <div class="line" />
+            </div>
+        </header>
+    );
+};
+
+export default Header;
