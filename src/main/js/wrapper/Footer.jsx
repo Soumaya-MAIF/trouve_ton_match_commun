504b39dea7e377b5de24fc0c4c94e1415dca8bee
Jerome BOUHET <jerome.bouhet@maif.fr>
2025-01-27 13:09:49 +0100

    chore: simplification du code dans wrapper, header et footer

--- src/main/js/wrapper/Footer.jsx
+++ src/main/js/wrapper/Footer.jsx
@@ -7,37 +7,35 @@ import './footer.css';
 const Footer = () => {
     return (
         <footer className='footer'>
-            <div className="footer-container">
-                <div className="row-footer">
-                    <div className="col-footer">
-                        <div className="social-icons">
-                            <div><img src={instagram} className="img-footer" alt="instagram" /></div>
-                            <div><img src={facebook} className="img-footer" alt="facebook" /></div>
-                            <div><img src={tiktok} className="img-footer" alt="tiktok" /></div>
-                            <div><img src={linkedin} className="img-footer" alt="linkedin" /></div>
-                        </div>
-                    </div>
-                    <div className="col-footer">
-                        <div className='Initiative79'><b>INITIATIVE DEUX SEVRES</b></div>
-                        <div>Pépinière d'Entreprises du Niortais</div>
-                        <div>4, BD Louis Tardy</div>
-                        <div>79000 Niort</div>
-                    </div>
-                    <div className="col-footer">
-                        <div>Téléphone : +33 6 79 87 56 09</div>
-                        <div>accompagnement@initiativedeuxsevres.fr</div>
-                    </div>
-                    <div className="col-footer">
-                        <div className='horaires'>
-                            <div>Du lundi au vendredi</div>
-                            <div>de 08h30 à 12h30</div>
-                            <div>et de 13h30 à 17h30</div>
-                        </div>
+            <div className="row-footer">
+                <div className="col-footer">
+                    <div className="social-icons">
+                        <div><img src={instagram} className="img-footer" alt="instagram" /></div>
+                        <div><img src={facebook} className="img-footer" alt="facebook" /></div>
+                        <div><img src={tiktok} className="img-footer" alt="tiktok" /></div>
+                        <div><img src={linkedin} className="img-footer" alt="linkedin" /></div>
                     </div>
                 </div>
-                <div className="mentionsLégales">
-                    <div>© 2025 Initiative France - Intranet</div>  
+                <div className="col-footer">
+                    <div className='Initiative79'><b>INITIATIVE DEUX SEVRES</b></div>
+                    <div>Pépinière d'Entreprises du Niortais</div>
+                    <div>4, BD Louis Tardy</div>
+                    <div>79000 Niort</div>
                 </div>
+                <div className="col-footer">
+                    <div>Téléphone : +33 6 79 87 56 09</div>
+                    <div>accompagnement@initiativedeuxsevres.fr</div>
+                </div>
+                <div className="col-footer">
+                    <div className='horaires'>
+                        <div>Du lundi au vendredi</div>
+                        <div>de 08h30 à 12h30</div>
+                        <div>et de 13h30 à 17h30</div>
+                    </div>
+                </div>
+            </div>
+            <div className="mentionsLégales">
+                <div>© 2025 Initiative France - Intranet</div>  
             </div>
         </footer>
     );
--- src/main/js/wrapper/Header.jsx
+++ src/main/js/wrapper/Header.jsx
@@ -8,65 +8,41 @@ const Header = () => {
     return (
         <header className="header">
             <div className="header-container">
-                <div className="header-row">
-                    <div className="header-col">
-                        <Link to="/">
-                            <div className="favicon-container">
-                                <div className="favicon" alt="favicon_TTM"></div>
-                            </div>
-                        </Link>
+                <Link to="/">
+                    <div className="favicon-container">
+                        <div className="favicon" alt="favicon_TTM"></div>
                     </div>
-                    <div className="header-col">
-                        <Link className="menu" to="/mon-compte-parrain">Mon compte</Link>
-                    </div>
-                    <div className="header-col">
-                        <Link className="menu" to="/profils">Profils disponibles</Link>
-                    </div>
-                    <div className="header-col">
-                        <Link className="menu" to="/messages">Messages</Link>
-                    </div>
-                    <div className="header-col">
-                        <Link className="menu" to="/matchs">Mes Matchs</Link>
-                    </div>
-                    <div className="header-col">
-                        <Link className="menu" to="/ressources">Ressources</Link>
-                    </div>
-                    <div className="header-col">
-                        <Link className="menu" to="/indicateurs">Indicateurs</Link>
-                    </div>
-                    <div className="header-col">
-                        <div className="bouton">
-                        {/* <button type="button" className="btn" onClick={logout}>Se déconnecter</button> */}
-                        <Link to="/connexion">
-                            <button type="button" className="btn-connexion">Se connecter</button>
-                        </Link>
-                            {/* {isAuthenticated ? (
-                                <button type="button" className="btn" onClick={logout}>Se déconnecter</button>
-                            ) : (
-                                <Link to="/connexion">
-                                    <button type="button" className="btn">Se connecter</button>
-                                </Link>
-                            )} */}
-                        </div>
-                    </div>
-                    <div className="header-col">
-                        <div className="bouton">
-                            <Link to="/creation-compte">
-                                <button type="button" className="btn-creer">Créer un compte</button>
+                </Link>
+                <Link className="menu" to="/mon-compte-parrain">Mon compte</Link>
+                <Link className="menu" to="/profils">Profils disponibles</Link>
+                <Link className="menu" to="/messages">Messages</Link>
+                <Link className="menu" to="/matchs">Mes Matchs</Link>
+                <Link className="menu" to="/ressources">Ressources</Link>
+                <Link className="menu" to="/indicateurs">Indicateurs</Link>
+                <div className="bouton">
+                    {/* <button type="button" className="btn" onClick={logout}>Se déconnecter</button> */}
+                    <Link to="/connexion">
+                        <button type="button" className="btn-connexion">Se connecter</button>
+                    </Link>
+                        {/* {isAuthenticated ? (
+                            <button type="button" className="btn" onClick={logout}>Se déconnecter</button>
+                        ) : (
+                            <Link to="/connexion">
+                                <button type="button" className="btn">Se connecter</button>
                             </Link>
-                        </div>
-                    </div>
+                            )} */}
                 </div>
-                <div className="header-row">
-                    <div className="header-col">
-                        <div className="logo-ttm" alt="logo_TTM" />
-                    </div>
-                    <div className="header-col">
-                        <div className="logo-reseau" alt="logo_reseau" />
-                    </div>
+                <div className="bouton">
+                    <Link to="/creation-compte">
+                        <button type="button" className="btn-creer">Créer un compte</button>
+                    </Link>
                 </div>
-                <div class="line" />
             </div>
+            <div className="header-container">
+                <div className="logo-ttm" alt="logo_TTM" />
+                <div className="logo-reseau" alt="logo_reseau" />
+            </div>
+            <div class="line" />
         </header>
     );
 };
