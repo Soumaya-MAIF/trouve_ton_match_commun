eb4d2bce92ad41958418668286a4d9baf970e6e3
Soumaya Belhachemi <soumaya.belhachemi@maif.fr>
2025-04-07 18:54:02 +0200

    Mise à jour back
    Le code commenté sera à supprimer

--- src/main/java/back/trouve_ton_match/entity/User.java
+++ src/main/java/back/trouve_ton_match/entity/User.java
@@ -16,6 +16,7 @@ import java.util.List;
 @Inheritance(strategy = InheritanceType.JOINED)
 public class  User {
 
+    @Getter
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     protected Long id;
@@ -32,92 +33,53 @@ public class  User {
 
     protected Role role =  Role.UTILISATEUR;
 
-    public Long getId() {
-        return id;
-    }
+//    public void setId(Long id) {
+//        this.id = id;
+//    }
+//
+//    public String getNom() {
+//        return nom;
+//    }
+//
+//    public void setNom(String nom) {
+//        this.nom = nom;
+//    }
+//
+//    public String getPrenom() {
+//        return prenom;
+//    }
+//
+//    public void setPrenom(String prenom) {
+//        this.prenom = prenom;
+//    }
 
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getNom() {
-        return nom;
-    }
-
-    public void setNom(String nom) {
-        this.nom = nom;
-    }
-
-    public String getPrenom() {
-        return prenom;
-    }
-
-    public void setPrenom(String prenom) {
-        this.prenom = prenom;
-    }
-
-    public String getEntreprise() {
-        return entreprise;
-    }
-
-    public void setEntreprise(String entreprise) {
-        this.entreprise = entreprise;
-    }
-
-    public String getPlateforme() {
-        return plateforme;
-    }
-
-    public void setPlateforme(String plateforme) {
-        this.plateforme = plateforme;
-    }
-
-    public String getCode_acces() {
-        return code_acces;
-    }
-
-    public void setCode_acces(String code_acces) {
-        this.code_acces = code_acces;
-    }
-
-    public Role getRole() {
-        return role;
-    }
-
-    public void setRole(Role role) {
-        this.role = role;
-    }
-
-    public List<TypeAccompagnement> getType_accompagnement() {
-        return typeAccompagnement;
-    }
-
-    public void setTypeAccompagnement(List<TypeAccompagnement> typeAccompagnement) {
-        this.typeAccompagnement = typeAccompagnement;
-    }
-
-    public List<SecteurReseau> getSecteurReseau() {
-        return secteursReseaux;
-    }
-
-    public void setSecteurReseau(List<SecteurReseau> secteursReseaux) {
-        this.secteursReseaux = secteursReseaux;
-    }
-
-    @ManyToMany
-    @JoinTable(
-            name = "users_secteurs_reseaux",
-            joinColumns = @JoinColumn(name = "users_id"),
-            inverseJoinColumns = @JoinColumn(name = "secteurs_reseaux_id")
-    )
-    private List<SecteurReseau> secteursReseaux;
-
-    @ManyToMany
-    @JoinTable(
-            name = "user_types_accompagnements",
-            joinColumns = @JoinColumn(name = "users_id"),
-            inverseJoinColumns = @JoinColumn(name = "types_accompagnements_id")
-    )
-    private List<TypeAccompagnement> typeAccompagnement;
+//    public List<TypeAccompagnement> getType_accompagnement() {
+//        return typeAccompagnement;
+//    }
+//
+//    public List<SecteurReseau> getSecteurReseau() {
+//        return secteursReseaus;
+//    }
+//
+//    public void setSecteurReseau(List<SecteurReseau> secteursReseaus) {
+//        this.secteursReseaus = secteursReseaus;
+//    }
+//
+//    @ManyToMany
+//    @JoinTable(
+//            name = "users_secteurs_reseaux",
+//            joinColumns = @JoinColumn(name = "users_id"),
+//            inverseJoinColumns = @JoinColumn(name = "secteurs_reseaux_id")
+//    )
+//    private List<SecteurReseau> secteursReseaus;
+//
+//    @Setter
+//    @ManyToMany
+//    @JoinTable(
+//            name = "user_types_accompagnements",
+//            joinColumns = @JoinColumn(name = "users_id"),
+//            inverseJoinColumns = @JoinColumn(name = "types_accompagnements_id")
+//    )
+//    private List<TypeAccompagnement> typeAccompagnement;
 
 }
--- src/main/java/back/trouve_ton_match/repository/SecteursReseauxRepository.java
+++ src/main/java/back/trouve_ton_match/repository/SecteursReseauxRepository.java
@@ -1,9 +1,9 @@
-package back.trouve_ton_match.repository;
-
-import back.trouve_ton_match.entity.SecteursReseaux;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface SecteursReseauxRepository extends JpaRepository<SecteursReseaux, Long> {
-}
+//package back.trouve_ton_match.repository;
+//
+//import back.trouve_ton_match.entity.SecteurReseau;
+//import org.springframework.data.jpa.repository.JpaRepository;
+//import org.springframework.stereotype.Repository;
+//
+//@Repository
+//public interface SecteursReseauxRepository extends JpaRepository<SecteurReseau, Long> {
+//}
